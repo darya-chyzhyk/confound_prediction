f86a6b482948867742d5b4eb07828a2c11686349
Darya <darya.chyzhyk@gmail.com>
2019-05-09 11:14:08 +0200

    add function to random sampling

--- confound_isolating/sampling.py
+++ confound_isolating/sampling.py
@@ -30,12 +30,12 @@ def random_index_2remove(y, z):
         train_test_split(y, z, np.arange(y.shape[0]), test_size=0.25,
                          random_state=42)
     #ratio_dens, kde_y, kde_z, kde_yz, scale_method = [0, 0, 0, 0, 0]
-    index_2remove = np.random.choice(index_test, 4, replace=False)
+    index_to_remove = np.random.choice(index_test, 4, replace=False)
 
     # TODO for the output keep just index_to_remove
 
     # TODO make number (4) of removing samples as parameter?
-    return index_2remove,
+    return index_to_remove,
 
 
 def confound_izolating_index_2remove(y, z, prng=None):
@@ -84,10 +84,47 @@ def confound_izolating_index_2remove(y, z, prng=None):
 
 
 
-def random_sampling(y, z, ids, type_sampling='sampling_cumsum',
-                            n_seed=0,
-             min_size=None, save_mi_iter=False, type_bandwidth='scott'):
-    print()
+def random_sampling(y, z, min_sample_size=None, type_bandwidth='scott'):
+
+
+    ids = list(range(0, y.shape[0]))
+
+    mi_list = []
+    corr_list = []
+    n_iter = 0
+    index_to_remove = []
+
+    array_data = np.c_[y, z, ids]
+    if min_sample_size is None:
+        min_size = np.int(y.shape[0] / 10)
+    else:
+        min_size = np.int(y.shape[0] * min_sample_size / 100)
+
+    while array_data.shape[0] > min_size:
+        n_iter = n_iter + 1
+        # remove subject from the previous iteration
+        array_data = np.delete(array_data, index_to_remove, axis=0)
+        y_sampling = array_data[:, 0]
+        z_sampling = array_data[:, 1]
+
+        # return indexes
+        index_to_remove = random_index_2remove(y, z)
+
+    # The case when target and confound are equal
+    if np.all(y_sampling == z_sampling) == True:
+        mi_list.append('NaN')
+    else:
+        mi_list.append(mutual_kde(y_sampling.astype(float),
+                                  z_sampling.astype(float),
+                                  type_bandwidth=type_bandwidth))
+    corr_list.append(np.corrcoef(y_sampling.astype(float),
+                                 z_sampling.astype(float))[0, 1])
+
+    sampled_set = {'sampled_index': array_data[:, 2],
+                   'mutual_information': mi_list,
+                   'correlation': corr_list}
+
+    return Bunch(**sampled_set)
 
 
 
@@ -101,9 +138,10 @@ def confound_izolating_sampling(y, z, n_seed=0, min_sample_size=None,
     :param y: numpy.array, shape (n_samples), target
     :param z: numpy.array, shape (n_samples), confound
     :param n_seed: int
-        defaul is '0'
-    :param min_sample_size: int
-        Minimum sample size to be reached, default is 'None'
+        Random seed used to initialize the pseudo-random number generator.
+        Can be any integer between 0 and 2**32 - 1 inclusive. Defaul is '0'
+    :param min_sample_size: float
+        Minimum sample size to be reached, default is 10% of the data
     :param type_bandwidth: str, scalar or callable, optional
         The method used to calculate the estimator bandwidth.  This can be
         'scott', '2scott', '05scott'
@@ -112,38 +150,20 @@ def confound_izolating_sampling(y, z, n_seed=0, min_sample_size=None,
 
     # TODO do we need 'type _bandwidth'?
 
-    '''
-    Sampling
-    :param y:
-    :param z:
-    :param ids:
-    :param type_sampling:
-    :param n_seeds:
-    :param min_size: float
-        size of sapled test, default is 10% of the data
-    :return:
-    '''
     ids = list(range(0, y.shape[0]))
 
     mi_list = []
     corr_list = []
-    mi_iter = []
-    corr_iter = []
-    seed_iter = []
-    n_subjects_list = []
-    y_sampling_list = []
-    z_sampling_list = []
-
     n_iter = 0
     index_to_remove = []
+
     array_data = np.c_[y, z, ids]
-    if min_size is None:
+    if min_sample_size is None:
         min_size = np.int(y.shape[0] / 10)
     else:
-        min_size = np.int(y.shape[0] * min_size / 100)
+        min_size = np.int(y.shape[0] * min_sample_size / 100)
 
     while array_data.shape[0] > min_size:
-
         n_iter = n_iter + 1
         # remove subject from the previous iteration
         array_data = np.delete(array_data, index_to_remove, axis=0)
@@ -154,15 +174,9 @@ def confound_izolating_sampling(y, z, n_seed=0, min_sample_size=None,
         prng = np.random.RandomState(seed=n_seed)
 
         # return indexes
-        index_to_remove = confound_izolating_index_2remove(y, z, prng=None)
-        # ratio_dens, index_to_remove, kde_y, kde_z, kde_yz, scale_method = \
-        #     sampling_with_kde(y_sampling, z_sampling, type_sampling, prng)
+        index_to_remove = confound_izolating_index_2remove(y, z, prng=prng)
 
-
-
-
-
-
+    # The case when target and confound are equal
     if np.all(y_sampling==z_sampling) == True:
         mi_list.append('NaN')
     else:
@@ -172,13 +186,85 @@ def confound_izolating_sampling(y, z, n_seed=0, min_sample_size=None,
     corr_list.append(np.corrcoef(y_sampling.astype(float),
                                  z_sampling.astype(float))[0, 1])
 
-
     sampled_set = {'sampled_index': array_data[:, 2],
                    'mutual_information': mi_list,
                    'correlation': corr_list}
 
-
     return Bunch(**sampled_set)
+
+
+
+def sampling_with_kde(x, y, type_sampling, prng=None):
+    # TODO split into 2 functions
+    # confound_izolating_sampling
+    '''
+
+    :param x: numpy.array
+    :param y: numpy.array
+    :param type_sampling: 'sampling_cumsum' - with kde,
+    :param prng:
+    :return:
+    '''
+    bandwidth = 'scott'
+    x_train, x_test, y_train, y_test, index_train, index_test = \
+        train_test_split(x, y, np.arange(x.shape[0]), test_size=0.25,
+                         random_state=42)
+
+    xy_train = np.array((x_train, y_train))
+    xy_test = np.array((x_test, y_test))
+
+    # Options for sampling
+
+    if type_sampling == 'sampling_cumsum':
+        # scaling for kde
+        scaler = preprocessing.StandardScaler()
+        scaler.fit(xy_train.T)
+        scale_method = scaler
+        train_scaled = scaler.transform(xy_train.T).T
+        test_scaled = scaler.transform(xy_test.T).T
+        kde_xy = gaussian_kde(train_scaled, bw_method=bandwidth)
+
+        # bandwidth: train and test on kde_xy, and use it for kde_x and kde_y
+        bandwidth_xy = kde_xy.factor
+        kde_x = gaussian_kde(train_scaled[0], bw_method=bandwidth_xy)
+        kde_y = gaussian_kde(train_scaled[1], bw_method=bandwidth_xy)
+
+        # Ratio, to find max
+        ratio_dens = (kde_xy(test_scaled)) / (kde_x(test_scaled[0]) * kde_y(
+            test_scaled[1]))  # + 1e-4
+
+
+    elif type_sampling == 'random_4':
+        ratio_dens, kde_x, kde_y, kde_xy, scale_method = [0,0,0,0,0]
+
+    # subjects to remove
+    if type_sampling == 'random_4':
+        index_test_remove = np.random.choice(index_test, 4, replace=False)
+    else:
+        # index_sort = np.argsort(-ratio_dens)
+        # ratio_sort = ratio_dens[np.argsort(-ratio_dens)]
+
+        index_sort = np.argsort(ratio_dens)
+        ratio_sort = ratio_dens[np.argsort(ratio_dens)]
+
+        empirical_cdf = (np.cumsum(ratio_sort)) ** 5
+        if prng is None:
+            random_quantiles = np.random.random(size=4) * empirical_cdf.max()
+        else:
+            random_quantiles = prng.rand(4) * empirical_cdf.max()
+        idx_to_reject = np.searchsorted(empirical_cdf, random_quantiles,
+                                        side='left')
+        ratio_remove = ratio_dens[index_sort[idx_to_reject]]
+        # index from test subset to remove
+        index_test_remove = index_test[index_sort[idx_to_reject]]
+    #print('Index in x_test to remove: ', index_test_remove)
+    #mask[index_test_remove] = 0
+    return ratio_dens, index_test_remove, kde_x, kde_y, kde_xy, scale_method
+
+
+
+# TODO move to example
+
 
 def confound_izolating_sampling_iterations(y, z, n_seed=0,
              min_size=None, save_mi_iter=False, type_bandwidth='scott'):
@@ -276,72 +362,3 @@ def confound_izolating_sampling_iterations(y, z, n_seed=0,
 
 
     return Bunch(**sampled_set), Bunch(**sampled_iter)
-
-
-def sampling_with_kde(x, y, type_sampling, prng=None):
-    # TODO split into 2 functions
-    # confound_izolating_sampling
-    '''
-
-    :param x: numpy.array
-    :param y: numpy.array
-    :param type_sampling: 'sampling_cumsum' - with kde,
-    :param prng:
-    :return:
-    '''
-    bandwidth = 'scott'
-    x_train, x_test, y_train, y_test, index_train, index_test = \
-        train_test_split(x, y, np.arange(x.shape[0]), test_size=0.25,
-                         random_state=42)
-
-    xy_train = np.array((x_train, y_train))
-    xy_test = np.array((x_test, y_test))
-
-    # Options for sampling
-
-    if type_sampling == 'sampling_cumsum':
-        # scaling for kde
-        scaler = preprocessing.StandardScaler()
-        scaler.fit(xy_train.T)
-        scale_method = scaler
-        train_scaled = scaler.transform(xy_train.T).T
-        test_scaled = scaler.transform(xy_test.T).T
-        kde_xy = gaussian_kde(train_scaled, bw_method=bandwidth)
-
-        # bandwidth: train and test on kde_xy, and use it for kde_x and kde_y
-        bandwidth_xy = kde_xy.factor
-        kde_x = gaussian_kde(train_scaled[0], bw_method=bandwidth_xy)
-        kde_y = gaussian_kde(train_scaled[1], bw_method=bandwidth_xy)
-
-        # Ratio, to find max
-        ratio_dens = (kde_xy(test_scaled)) / (kde_x(test_scaled[0]) * kde_y(
-            test_scaled[1]))  # + 1e-4
-
-
-    elif type_sampling == 'random_4':
-        ratio_dens, kde_x, kde_y, kde_xy, scale_method = [0,0,0,0,0]
-
-    # subjects to remove
-    if type_sampling == 'random_4':
-        index_test_remove = np.random.choice(index_test, 4, replace=False)
-    else:
-        # index_sort = np.argsort(-ratio_dens)
-        # ratio_sort = ratio_dens[np.argsort(-ratio_dens)]
-
-        index_sort = np.argsort(ratio_dens)
-        ratio_sort = ratio_dens[np.argsort(ratio_dens)]
-
-        empirical_cdf = (np.cumsum(ratio_sort)) ** 5
-        if prng is None:
-            random_quantiles = np.random.random(size=4) * empirical_cdf.max()
-        else:
-            random_quantiles = prng.rand(4) * empirical_cdf.max()
-        idx_to_reject = np.searchsorted(empirical_cdf, random_quantiles,
-                                        side='left')
-        ratio_remove = ratio_dens[index_sort[idx_to_reject]]
-        # index from test subset to remove
-        index_test_remove = index_test[index_sort[idx_to_reject]]
-    #print('Index in x_test to remove: ', index_test_remove)
-    #mask[index_test_remove] = 0
-    return ratio_dens, index_test_remove, kde_x, kde_y, kde_xy, scale_method
-
