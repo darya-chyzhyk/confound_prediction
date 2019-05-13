0f21fa3d5bb5ca6b8e125ed6139225b49cc54fde
Darya <darya.chyzhyk@gmail.com>
2019-05-13 14:01:09 +0200

    cleaning

--- example/simulated_data.py
+++ example/simulated_data.py
@@ -36,3 +36,72 @@ else:
                           type_sampling=type_sampling, n_seed=n_seed,
                           min_size=min_size, save_mi_iter=save_mi_iter)
         for n_seed in range(n_seeds))
+
+
+# clear signal
+
+
+
+def _standardize(signals, detrend=False, normalize=True):
+    """ Center and norm a given signal (time is along first axis)
+
+    Parameters
+    ----------
+    signals: numpy.ndarray
+        Timeseries to standardize
+
+    detrend: bool
+        if detrending of timeseries is requested
+
+    normalize: bool
+        if True, shift timeseries to zero mean value and scale
+        to unit energy (sum of squares).
+
+    Returns
+    -------
+    std_signals: numpy.ndarray
+        copy of signals, normalized.
+    """
+
+
+
+    if normalize:
+        if signals.shape[0] == 1:
+            warnings.warn('Standardization of 3D signal has been requested but '
+                'would lead to zero values. Skipping.')
+            return signals
+
+        if not detrend:
+            # remove mean if not already detrended
+            signals = signals - signals.mean(axis=0)
+
+        std = np.sqrt((signals ** 2).sum(axis=0))
+        std[std < np.finfo(np.float).eps] = 1.  # avoid numerical problems
+        signals /= std
+    return signals
+
+
+
+
+def clean_conf(confounds):
+    confounds = _ensure_float(confounds)
+
+    # Apply low- and high-pass filters to keep filters orthogonal
+    # (according to Lindquist et al. (2018))
+
+
+    confounds = _standardize(confounds, normalize=standardize,
+                             detrend=detrend)
+
+    if not standardize:
+        # Improve numerical stability by controlling the range of
+        # confounds. We don't rely on _standardize as it removes any
+        # constant contribution to confounds.
+        confound_max = np.max(np.abs(confounds), axis=0)
+        confound_max[confound_max == 0] = 1
+        confounds /= confound_max
+
+    # Pivoting in qr decomposition was added in scipy 0.10
+    Q, R, _ = linalg.qr(confounds, mode='economic', pivoting=True)
+    Q = Q[:, np.abs(np.diag(R)) > np.finfo(np.float).eps * 100.]
+    signals -= Q.dot(Q.T).dot(signals)
