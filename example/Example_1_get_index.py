00dc03c431f31eff54bee673a8fcd99a6e91eba0
Darya <darya.chyzhyk@gmail.com>
2019-05-21 07:18:15 +0200

    testing

--- example/Example_1_get_index.py
+++ example/Example_1_get_index.py
@@ -3,11 +3,14 @@
 from confound_isolating.data_simulation import simulate_confounded_data
 from confound_isolating.sampling import (random_index_2remove,
                                          confound_isolating_index_2remove,
-                                         confound_isolating_sampling)
+                                         confound_isolating_sampling,
+                                         random_sampling)
 
 # Simulate data
 X, y, z, = simulate_confounded_data(link_type='direct_link', n_samples=100,
                           n_features=100)
+
+
 ids = list(range(0, y.shape[0]))
 
 # Remove index with random or Confound Izolating methods
@@ -19,8 +22,17 @@ ids = list(range(0, y.shape[0]))
 # ratio_dens, ratio_remove, index_test_remove, kde_y, kde_z, kde_yz = \
 #     confound_isolating_index_2remove(y, z)
 
-sampled_set = confound_isolating_sampling(y, z, n_seed=0, min_sample_size=None,
+
+
+ind_ci, mi_ci, cor_ci = confound_isolating_sampling(y, z, n_seed=0,
+                                            min_sample_size=None,
                                 type_bandwidth='scott')
+
+a, b, c = random_sampling(y, z, min_sample_size=None, type_bandwidth='scott')
+
+
+
+
 
 
 
