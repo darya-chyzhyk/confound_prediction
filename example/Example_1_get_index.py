c6376c2364b8e1771c93fef4bc32cfd824479fc3
Darya <darya.chyzhyk@gmail.com>
2019-05-23 05:46:07 +0200

    testing

--- example/Example_1_get_index.py
+++ example/Example_1_get_index.py
@@ -6,12 +6,39 @@ from confound_isolating.sampling import (random_index_2remove,
                                          confound_isolating_sampling,
                                          random_sampling)
 
+from confound_isolating.deconfounding import (confound_isolating_cv,
+                                              confound_regressout)
+
+
+
 # Simulate data
 X, y, z, = simulate_confounded_data(link_type='direct_link', n_samples=100,
-                          n_features=100)
+                          n_features=2)
+
+x_test, x_train, y_test, y_train, ids_test, ids_train = \
+    confound_isolating_cv(X, y, z, random_seed=0, min_sample_size=None,
+                          cv_folds=10, type_bandwidth='scott')
 
 
-ids = list(range(0, y.shape[0]))
+
+x_test, x_train, y_test, y_train, ids_test, ids_train = \
+    confound_regressout(X, y, z, type_deconfound='out_of_sample',
+                        min_sample_size=None,
+                        type_bandwidth='scott')
+
+
+
+
+
+
+# a, b, c = random_sampling(y, z, min_sample_size=30, type_bandwidth='2scott')
+#
+# ind_ci, mi_ci, cor_ci = confound_isolating_sampling(y, z, n_seed=0,
+#                                             min_sample_size=None,
+#                                 type_bandwidth='scott')
+
+
+# ids = list(range(0, y.shape[0]))
 
 # Remove index with random or Confound Izolating methods
 
@@ -24,15 +51,7 @@ ids = list(range(0, y.shape[0]))
 
 
 
-ind_ci, mi_ci, cor_ci = confound_isolating_sampling(y, z, n_seed=0,
-                                            min_sample_size=None,
-                                type_bandwidth='scott')
 
-a, b, c = random_sampling(y, z, min_sample_size=None, type_bandwidth='scott')
-
-
-
-
-
+# a, b, c = random_sampling(y, z, min_sample_size=None, type_bandwidth='scott')
 
 
