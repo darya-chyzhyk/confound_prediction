629f9dc7d6794f2df90ac75810a9245cb1e2e923
Darya <darya.chyzhyk@gmail.com>
2019-06-01 16:54:17 +0200

    docstrings

--- confound_isolating/utils.py
+++ confound_isolating/utils.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 def _ensure_int_positive(var, default=None):
+    """
+    Function that ensure that parameter is positive and integer and if it is
+    None change it to default value
+    :param var:
+    :param default:
+    :return:
+    """
 
 
     if var is None:
