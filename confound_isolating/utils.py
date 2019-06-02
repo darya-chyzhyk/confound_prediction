97bc865752fc239325db7ce13c97ea38ca56eedf
Darya <darya.chyzhyk@gmail.com>
2019-06-02 06:55:40 +0200

    create function to check the parameters

--- confound_isolating/utils.py
+++ confound_isolating/utils.py
@@ -1,27 +1,43 @@
 import numpy as np
 
-def _ensure_int_positive(var, default=None):
+
+def _ensure_int_positive(data, default=None):
     """
-    Function that ensure that parameter is positive and integer and if it is
-    None change it to default value
-    :param var:
+    Function that ensure that data is positive and integer.
+    If data is None change it to default value.
+    If the data is float, convert it to int.
+    :param data:
     :param default:
     :return:
     """
 
-
-    if var is None:
+    if data is None:
         if default is None:
-            raise TypeError(var + ' or ' + default + " must be positive "
+            raise TypeError(data + ' or ' + default + " must be positive "
                                                      "integer")
         else:
-            var = default
-
+            data = default
     else:
-        if isinstance(var, (list, tuple, str, np.ndarray)) or var < 0:
-            raise TypeError(var + " keyword has an unhandled type: %s"
-                            % var.__class__ + " or it is not positive")
+        if isinstance(data, (list, tuple, str, np.ndarray)) or data < 0:
+            raise TypeError(data + " keyword has an unhandled type: %s"
+                            % data.__class__ + " or it is not positive")
 
-        if isinstance(var, float):
-            var = int(var)
-    return var
+        if isinstance(data, float):
+            data = int(data)
+    return data
+
+
+a = _ensure_int_positive(5.1, default=None)
+print(a)
+
+a = _ensure_int_positive(-1, default=None)
+print(a)
+
+a = _ensure_int_positive(2, default=None)
+print(a)
+
+a = _ensure_int_positive(None, default=None)
+print(a)
+
+a = _ensure_int_positive(None, default=4)
+print(a)
