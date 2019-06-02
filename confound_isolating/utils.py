d9deccaa98688bc00b4cff588ffb784421c55904
Darya <darya.chyzhyk@gmail.com>
2019-06-02 17:16:26 +0200

    test the parameter

--- confound_isolating/utils.py
+++ confound_isolating/utils.py
@@ -13,31 +13,16 @@ def _ensure_int_positive(data, default=None):
 
     if data is None:
         if default is None:
-            raise TypeError(data + ' or ' + default + " must be positive "
+            raise TypeError(str(data) + ' or ' + default + " must be positive "
                                                      "integer")
         else:
             data = default
     else:
         if isinstance(data, (list, tuple, str, np.ndarray)) or data < 0:
-            raise TypeError(data + " keyword has an unhandled type: %s"
+            raise TypeError(str(data) + " keyword has an unhandled type: %s"
                             % data.__class__ + " or it is not positive")
 
         if isinstance(data, float):
             data = int(data)
     return data
 
-
-a = _ensure_int_positive(5.1, default=None)
-print(a)
-
-a = _ensure_int_positive(-1, default=None)
-print(a)
-
-a = _ensure_int_positive(2, default=None)
-print(a)
-
-a = _ensure_int_positive(None, default=None)
-print(a)
-
-a = _ensure_int_positive(None, default=4)
-print(a)
