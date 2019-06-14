5338077fb4f26a2f543b43a9a9daceaea2041c3a
Darya <darya.chyzhyk@gmail.com>
2019-06-14 09:33:46 +0200

    testing

--- example/Example-prediction.py
+++ example/Example-prediction.py
@@ -2,7 +2,8 @@
 Example of prediction
 """
 
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 from confound_prediction.data_simulation import simulate_confounded_data
 from confound_prediction.sampling import (random_index_2remove,
@@ -18,13 +19,14 @@ from sklearn.metrics import (mean_squared_error, mean_absolute_error,
 
 
 def model_fit_datasplit(x_train_cv, x_test_cv, y_train_cv, y_test_cv, model):
-
+    mse = []
+    mae = []
+    evs = []
+    r2s = []
     for x_train, x_test, y_train, y_test in zip(x_train_cv, x_test_cv,
                                                 y_train_cv, y_test_cv):
-        mse = []
-        mae = []
-        evs = []
-        r2s = []
+        print('Start prediction with ', model)
+
 
         model.fit(x_train, y_train)
         test_predict = model.predict(x_test)
@@ -71,8 +73,60 @@ mse_cicv, mae_cicv, evs_cicv, r2s_cicv = \
     model_fit_datasplit(x_test_cicv, x_train_cicv, y_test_cicv, y_train_cicv,
                         model)
 
+mse_oos, mae_oos, evs_oos, r2s_oos = \
+    model_fit_datasplit(x_test_oos, x_train_oos, y_test_oos, y_train_oos, model)
+
+mse_ma, mae_ma, evs_ma, r2s_ma = \
+    model_fit_datasplit(x_test_ma, x_train_ma, y_test_ma, y_train_ma, model)
+
+mse_fa, mae_fa, evs_fa, r2s_fa = model_fit_datasplit(x_test_fa, x_train_fa, y_test_fa,
+                                         y_train_fa, model)
+
+
+mae_plot = [np.array(mae_cicv), np.array(mae_oos), np.array(mae_ma),
+            np.array(mae_fa)]
+
+r2s_plot = [np.array(r2s_cicv), np.array(r2s_oos), np.array(r2s_ma),
+            np.array(r2s_fa)]
 
 # Plotting
+
+
+
+
+labels = ['Confound isolation', 'Out-of-sample', 'Model-agnostic',
+          'Without deconfounding']
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+
+# rectangular box plot
+bplot1 = axes[0].boxplot(mae_plot,
+                         vert=True,  # vertical box alignment
+                         patch_artist=True,  # fill with color
+                         labels=labels)  # will be used to label x-ticks
+axes[0].set_title('Rectangular box plot')
+
+# notch shape box plot
+bplot2 = axes[1].boxplot(r2s_plot,
+                         notch=True,  # notch shape
+                         vert=True,  # vertical box alignment
+                         patch_artist=True,  # fill with color
+                         labels=labels)  # will be used to label x-ticks
+axes[1].set_title('Comparison of prediction')
+
+# fill with colors
+colors = ['firebrick', 'olive', 'orange', 'steelblue']
+for bplot in (bplot1, bplot2):
+    for patch, color in zip(bplot['boxes'], colors):
+        patch.set_facecolor(color)
+
+# adding horizontal grid lines
+for ax in axes:
+    ax.yaxis.grid(True)
+    ax.set_xlabel('Three separate samples')
+    ax.set_ylabel('Observed values')
+
+plt.show()
 
 
 
